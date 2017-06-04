25dd9a684516e527915bf4486dff2efdbc27d278
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2017-06-04 01:29:53 +0100

    + Removed logging from Live page + Added Teaching page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit, Input} from "@angular/core";
 import {MenuItemComponent} from "../menu-item/menu-item.component";
 import {DataServiceService} from "../data-service.service";
+import {animateFactory} from 'ng2-animate';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css', '/animate.css', './hamburgers.css'],
+  animations: [animateFactory(1000, 500, "fadeIn"), animateFactory(1000, 500, "fadeOut")]
 })
 export class MenuComponent implements OnInit {
   menuItems: MenuItemComponent[];
   @Input() active: string;
+  showStyle: false;
+  state: string = 'fadeIn';
 
   constructor(private dataService: DataServiceService) {
     this.menuItems = dataService.getMenuItems();
@@ -19,6 +23,11 @@ export class MenuComponent implements OnInit {
     //Set the active tab.
     this.changeActive(this.active);
   }
+
+  changeState() {
+    this.state == 'fadeIn' ? this.state = 'fadeOut' : this.state = 'fadeIn';
+  }
+
 
   changeActive(title: string) {
     for (let menuItem of this.menuItems) {
