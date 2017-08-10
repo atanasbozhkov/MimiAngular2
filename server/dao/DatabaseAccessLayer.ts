29b2d4cc1d36242d317012f5c28722aaf11bd668
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2017-08-10 23:52:50 +0100

    Responsive version (#14)
    * + Added social network on the contact page.
    
    * + Removed logging from Live page
    + Added Teaching page
    
    * + Removed logging from Live page
    + Added Teaching page
    
    * + Finished responsive home page and menu.
    
    * + About page responsive
    
    * + Menu will now scroll top top on click
    + Gallery, About, Music and Live are responsive.
    
    * + All pages are responsive
    + Teaching page "Coming soon" added
    
    * + Removing old event.
    
    * + Reverting package json

--- server/dao/DatabaseAccessLayer.ts
+++ server/dao/DatabaseAccessLayer.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import * as Q from "@types/q";
+import * as Q from "q";
 import {LivePage} from "./LivePage";
 import {Pages} from "./Pages";
 import {HomePageData} from "./types/HomePageData";
