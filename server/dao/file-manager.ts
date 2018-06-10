1ac28e1ffcec4ab85fc5558bc29ec2f99a583bad
atanasbozhkov <bozhkov.atanas@gmail.com>
2018-06-11 00:38:20 +0100

    - Startup checks

--- server/dao/file-manager.ts
+++ server/dao/file-manager.ts
@@ -2,13 +2,33 @@ import {IFileManager, File, AssetType, FileName} from './interfaces/ifile-manage
 import * as fs from 'fs';
 
 export class FileManager implements IFileManager {
-
   private removeMe: File;
   // TODO: Read this from environment var
-  private readonly ASSET_PATH: string = '../';
+  private readonly ASSET_PATH: string = '../mimi-assets/';
 
-  public static sanitizeImageData(data: Buffer) {
+  public static sanitizeImageData(data: Buffer): string {
+    // TODO: Nasco - remove the `png` from string.
     return data.toString().replace(/^data:image\/png;base64,/, '');
+  }
+
+  constructor() {
+    this.startupCheck();
+  }
+
+  private startupCheck() {
+    if (!fs.existsSync(this.ASSET_PATH)) {
+      console.log('Asset path not detected - attempting to create it');
+      fs.mkdirSync(this.ASSET_PATH);
+    }
+    // Check and create asset-type directories if necessary
+    for (const assetTypeKey of Object.keys(AssetType)) {
+      const assetTypeVal = AssetType[assetTypeKey];
+      const assetSubDir = this.ASSET_PATH + assetTypeVal;
+      if (!fs.existsSync(assetSubDir)) {
+        console.log(`Asset sub directory ${assetSubDir} not found - attempting to create it.`);
+        fs.mkdirSync(assetSubDir)
+      }
+    }
   }
 
   listFiles(assetType: AssetType): Array<FileName> {
@@ -18,7 +38,6 @@ export class FileManager implements IFileManager {
   readFile(id: FileName, assetType: AssetType): File {
     const data = fs.readFileSync(this.ASSET_PATH + assetType + '/' + id);
     console.log(`Read data`);
-    console.log(data);
     const [fileName, extension ] = id.split('.');
     return new File(fileName, extension, data);
   }
