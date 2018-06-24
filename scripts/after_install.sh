64fb62d5a23f3e224170b3fca83693f3f515b20c
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2018-06-24 17:33:52 +0100

    Modified AWS deployment spec+scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Code in place. Starting NPM build"
 rm -r /tmp/
-cd /home/ubuntu/test
+cd /home/ubuntu/build
 npm install --no-optional
 npm run build
+mv -v dist/* /home/ubuntu/website/
+
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Starting Server"
-cd /home/ubuntu/test
-export NODE_ENV=production
-export PORT=80
-forever start dist/server/bin/www.js
+#echo "Starting Server"
+#cd /home/ubuntu/test
+#export NODE_ENV=production
+#export PORT=80
+#forever start dist/server/bin/www.js
